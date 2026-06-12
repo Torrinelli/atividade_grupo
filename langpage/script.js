615223281a48fcbccac7e0adefcd45ca71f95831
dev vini <viniciuskbaumann@gmail.com>
2026-06-11 21:53:01 -0300

    pagina inicial versão01 langpage

---
+++ langpage/script.js
@@ -0,0 +1,7 @@
+function abrirMenu() {
+    document.getElementById("sidebar").classList.add("ativo");
+}
+
+function fecharMenu() {
+    document.getElementById("sidebar").classList.remove("ativo");
+}
