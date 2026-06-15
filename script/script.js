e3e541fe907f18e379663aa30d2eda413dbbcd7e
Aluno SENAI <Aluno@SN-303864.edu.fiesc.com.br>
2026-06-15 19:22:25 -0300

    styler created

--- script/script.js
+++ script/script.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
-  $(window).on('scroll', function() {
-    var alturaJanela = $(window).height();
-    var scrollPos = $(window).scrollTop();
-
-    $('.timeline-item').each(function() {
-      var offsetItem = $(this).offset().top;
-
-      // Verifica se o item está visível na tela
-      if (offsetItem < scrollPos + alturaJanela - 100) {
-        $(this).find('.timeline-conteudo').animate({'opacity': '1'}, 800);
-      }
-    });
-  });
+    // Função para verificar se o card entrou na tela
+    function checkScroll() {
+        var windowHeight = $(window).height();
+        var scrollTop = $(window).scrollTop();
+        
+        $('.timeline-item').each(function() {
+            // Pega a posição do card
+            var elementOffset = $(this).offset().top;
+            
+            // Se o topo da tela + altura da janela ultrapassar o card, ele aparece
+            if (elementOffset < (scrollTop + windowHeight - 100)) {
+                $(this).addClass('visible');
+            }
+        });
+    }
+    
+    // Roda a função quando a página carrega (para mostrar os primeiros cards)
+    checkScroll();
+    
+    // Roda a função sempre que o usuário descer a página
+    $(window).on('scroll', checkScroll);
 });
