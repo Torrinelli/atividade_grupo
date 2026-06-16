5080ca6ae8e6bcf31ed2bb7fc57ba35cce15b7e8
Aluno SENAI <Aluno@SN-303864.edu.fiesc.com.br>
2026-06-15 21:52:31 -0300

    botão play

--- pag1/script/script.js
+++ pag1/script/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     // Função para verificar se o card entrou na tela
     function checkScroll() {
-        var windowHeight = $(window).height();
-        var scrollTop = $(window).scrollTop();
+        let windowHeight = $(window).height();
+        let scrollTop = $(window).scrollTop();
         
         $('.timeline-item').each(function() {
             // Pega a posição do card
-            var elementOffset = $(this).offset().top;
+            let elementOffset = $(this).offset().top;
             
             // Se o topo da tela + altura da janela ultrapassar o card, ele aparece
             if (elementOffset < (scrollTop + windowHeight - 100)) {
@@ -14,10 +14,36 @@ $(document).ready(function() {
             }
         });
     }
-    
+
     // Roda a função quando a página carrega (para mostrar os primeiros cards)
     checkScroll();
     
     // Roda a função sempre que o usuário descer a página
     $(window).on('scroll', checkScroll);
+});
+
+    // Target by ID
+$("#meuBotao").on("click", function() {
+    let video = $('.body-bg-video')[0];
+    let botao = $(this);
+
+    if(video.muted || video.paused){
+
+        video.muted = false; //tira o mudo
+        video.play(); //forçar o video a tocar (evita bloqueio do navegador)
+        
+        botao.text('Pause Music') // para mudar o texto do botão
+    }
+    else{
+
+        video.muted = true // colocar no mudo novamente
+        botao.text('Play Music'); //volta o texto original
+        
+    }
+    
+});
+
+// Target by Class
+$(".button").on("click", function() {
+    console.log("Submitted");
 });
